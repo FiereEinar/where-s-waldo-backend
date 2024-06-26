e84b17150d19e37173231a409882bcc9d60c8b56
FiereEinar <nickxylanmelloria@gmail.com>
2024-06-27 07:53:18 +0800

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 
 //controllers
 const scoresRouter = require('./routes/scores');
